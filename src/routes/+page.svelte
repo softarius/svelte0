974f41693a1623bf9e072e14aba0702e86b704c2
Sergey Yushinin <yushinins@mail.ru>
2026-04-23 21:32:50 +0300

    Ссылка на встривание кода

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Учебный проект SvelteKit</h1>
-<p>Перейдите <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> для чтения документации</p>
+<p>
+    Перейдите <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> для чтения
+    документации
+</p>
+<p>
+    <a href="https://apop.tech/posts/adding-svelte-to-an-existing-website/"
+        >Встраивание в код</a
+    >
+</p>
