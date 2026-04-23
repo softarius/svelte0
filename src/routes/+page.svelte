f2cb6a7c7eb71548ceb481dbf2a7451a6f6ad80c
Sergey Yushinin <yushinins@mail.ru>
2026-04-23 21:26:17 +0300

    Перевод главной страницы

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Учебный проект SvelteKit</h1>
+<p>Перейдите <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> для чтения документации</p>
